1dc790a124732f58fd23bf5108ac2bd5ab81ab1c
kathode <pfribeiro@gmail.com>
2017-10-20 17:23:02 +0100

    Update refines-json.sh
    Added correct timer implementation.

--- refines-json.sh
+++ refines-json.sh
@@ -18,7 +18,8 @@ fi
 # where we add a carriage-return to lines with the same first work,
 # such as in the case of "Constructed X states and Y transitions".
 
-# It also displays a timer for convenience!
+# It also displays a timer for convenience! 
+# (this requires gawk as systime() is not in the standard for awk)
 
 # By default it redirects the 'json' output to the second argument.
 # This is necessary, because the output with format 'colour' or 'plain'
@@ -29,7 +30,12 @@ echo "Running FDR on file '$1', outputing json into '$2' and diagnostings into '
 
 $REFINES --format json $1 2>&1 >$2 | awk 'BEGIN { prev = ""; start = systime();
                           while (getline) {
-                                if ($1 != prev) { printf("%s : %s",strftime("%H:%M:%S",systime()-start), $0); }
-                                else { printf("%s : %s\r",strftime("%H:%M:%S",systime()-start),$0); }
+                                delay = systime()-start;
+                                days = delay/60/60/24;
+                                hours = delay/60/60%24;
+                                minutes = delay/60%60;
+                                seconds = delay%60;
+                                if ($1 != prev) { printf("%02d-%02d:%02d:%02d : %s\n",days,hours,minutes,seconds, $0); } 
+                                else { printf("%02d-%02d:%02d:%02d : %s\r",days,hours,minutes,seconds, $0); }
                                 prev = $1
                         } }'
